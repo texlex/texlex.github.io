94d70f78b3ee947a6f5e76788f3a8de421149f63
MrZmann <reed.z@utexas.edu>
2020-08-25 21:38:57 -0500

    decoration

--- js/main.js
+++ js/main.js
@@ -5,18 +5,26 @@ var typewriter = new Typewriter(app, {
 });
 
 typewriter
+  .typeString("Hello! My name is ")
+  .pauseFor(25)
   .typeString(
-    'Hello! My name is <span style="color:blue">Mark Zimmermann</span>.'
+    '<span style="color:var(--red); font-weight:bold;">Mark Zimmermann</span>.'
   )
   .pauseFor(2500)
   .deleteAll()
-  .typeString('I am a <span style="color:green">litigator</span>.')
+  .typeString(
+    'I am a <span style="color:var(--blue); font-weight:bold;">litigator</span>.'
+  )
   .pauseFor(500)
   .deleteChars(10)
-  .typeString('<span style="color:green">trademark attorney</span>.')
+  .typeString(
+    '<span style="color:var(--blue); font-weight:bold;">trademark attorney</span>.'
+  )
   .pauseFor(1000)
   .deleteChars(25)
-  .typeString('<span style="color:red">fight</span> for my clients.')
+  .typeString(
+    '<span style="color:red; font-weight:bold;">fight</span> for my clients.'
+  )
   .pauseFor(2000)
   .start();
 
